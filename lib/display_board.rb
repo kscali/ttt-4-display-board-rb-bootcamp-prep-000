8c550f3cf398f6a485802ec4b37cbac199dcf156
kosatun@aol.com <kosatun@aol.com>
2018-07-11 09:16:18 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,11 +3,7 @@
 board = [" "," "," "," "," "," "," "," "," "]
 def display_board(board) 
   
-  puts " #{board[]) |     |   "
-  puts "-----------"
-  puts " #{board[])  |   |   "
-  puts "-----------"
-  puts " #{board[])  |   |   "
+  
 
   
 end  
